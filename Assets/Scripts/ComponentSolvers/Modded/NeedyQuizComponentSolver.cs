32c2accf89e94a71b8911e2478d0a20be0622932
caitsith2 <d_good@caitsith2.com>
2017-04-23 04:31:34 -0700

    Making use of the sendtochat command to indicate the the user said yes to Abort.

--- Assets/Scripts/ComponentSolvers/Modded/NeedyQuizComponentSolver.cs
+++ Assets/Scripts/ComponentSolvers/Modded/NeedyQuizComponentSolver.cs
@@ -10,6 +10,7 @@ public class NeedyQuizComponentSolver : ComponentSolver
     {
         _yesButton = (MonoBehaviour)_yesButtonField.GetValue(bombComponent.GetComponent(_componentSolverType));
         _noButton = (MonoBehaviour)_noButtonField.GetValue(bombComponent.GetComponent(_componentSolverType));
+        _display = (TextMesh) _displayField.GetValue(bombComponent.GetComponent(_componentSolverType));
     }
 
     protected override IEnumerator RespondToCommandInternal(string inputCommand)
@@ -20,6 +21,11 @@ public class NeedyQuizComponentSolver : ComponentSolver
             inputCommand.Equals("press yes", StringComparison.InvariantCultureIgnoreCase))
         {
             yield return "yes";
+
+            if (_display.text.Equals("Abort?"))
+            {
+                yield return "sendtochat ABORT! ABORT!!! ABOOOOOOORT!!!!!";
+            }
 
             DoInteractionStart(_yesButton);
             yield return new WaitForSeconds(0.1f);
@@ -43,12 +49,15 @@ public class NeedyQuizComponentSolver : ComponentSolver
         _componentSolverType = ReflectionHelper.FindType("AdvancedVentingGas");
         _yesButtonField = _componentSolverType.GetField("YesButton", BindingFlags.Public | BindingFlags.Instance);
         _noButtonField = _componentSolverType.GetField("NoButton", BindingFlags.Public | BindingFlags.Instance);
+        _displayField = _componentSolverType.GetField("Display", BindingFlags.Public | BindingFlags.Instance);
     }
 
     private static Type _componentSolverType = null;
     private static FieldInfo _yesButtonField = null;
     private static FieldInfo _noButtonField = null;
+    private static FieldInfo _displayField = null;
 
     private MonoBehaviour _yesButton = null;
     private MonoBehaviour _noButton = null;
+    private TextMesh _display = null;
 }
